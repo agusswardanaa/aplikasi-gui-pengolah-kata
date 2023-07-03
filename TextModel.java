b32aa945b67f51a5ee011f5f7123642865d11a35
I Bagus Putu Swardanasuta <agusswardana123@gmail.com>
2023-07-04 06:50:35 +0700

    Tugas 10

---
+++ TextModel.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PraPertemuan;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author hp14s
+ */
+public class TextModel implements Serializable {
+    private String text;
+    
+    public String getText() {
+        return text;
+    }
+    
+    public void setText(String text) {
+        this.text = text;
+    }
+}
